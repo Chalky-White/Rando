203164dbbf1725a69343d047d345a0736b56b53c
Benjamin L <benjamin.leboucher@consertotech.pro>
2018-09-28 10:06:41 +0200

    1st

---
+++ quickstart-master/src/app/hike/hike-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Hike } from './hike';
+
+
+@Component({
+    moduleId: module.id,
+    selector: 'hike-list', //<hike-list>
+    templateUrl: 'hike-list.component.html'
+})
+
+
+export class HikeListComponent { }
